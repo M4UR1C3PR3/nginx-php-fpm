b1c4f23b721762b4e5a41d94935861db697d5abb
Maurice Preuß <maurice@preuss.io>
2016-08-07 11:20:22 +0200

    Update start.sh
    Allow to force git pull

--- scripts/start.sh
+++ scripts/start.sh
@@ -29,7 +29,7 @@ if [ ! -z "$GIT_NAME" ]; then
 fi
 
 # Dont pull code down if the .git folder exists
-if [ ! -d "/var/www/html/.git" ]; then
+if [ "$FORCE_GIT_PULL" == "1" || ! -d "/var/www/html/.git" ]; then
  # Pull down code from git for our site!
  if [ ! -z "$GIT_REPO" ]; then
    # Remove the test index file
